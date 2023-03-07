1bf405525f740107ae1fe4ff9f90ee1a6e777465
Eduardo Luna <eduardoandreslp@ufps.edu.co>
2023-03-07 09:47:25 -0500

    Co-authored-by: Daniel A. Esteban <andresstbn@users.noreply.github.com>

--- src/EduStrings.vue
+++ src/EduStrings.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class = "my-4">
- <h6>Hola desde mi primer string <span class = text-primary >{{ strings }}</span></h6>
- <div class = "btn-group">
-<button class = "btn btn-danger" @click="() => strings += '1'"> Agregar </button>
-<button class = "btn btn-danger" @click="() => strings = '1'">Reiniciar</button>
-    </div>
+    <div class="my-4">
+        <h6>Hola desde mi primer string <span class=text-primary>{{ strings }}</span></h6>
+        <div class="btn-group">
+            <button class="btn btn-danger" @click="() => strings += '1'"> Agregar </button>
+            <button class="btn btn-danger" @click="() => strings = '1'"> Reiniciar</button>
+        </div>
     </div>
 </template>
 
@@ -14,6 +14,4 @@ import { ref } from 'vue';
 const strings = ref("1")
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
